a89ff035aa4b1be73b8f4898d9560b353c25827a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-10-28 09:25:54 +0800

    kernel: apk_sign: add more size/hash pairs
    this will make it support
    - this repo's manager
    - official manager, but I guess up to 1.0.1 for non-gki
    - 5ec1cff's MKSU
    - kowx712's KowSU

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -353,6 +353,7 @@ bool is_manager_apk(char *path)
 
 	return (check_v2_signature(path, 0x363, "4359c171f32543394cbc23ef908c4bb94cad7c8087002ba164c8230948c21549") // dummy.keystore
 	|| check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH)  // ksu official
-/*	|| check_v2_signature(path, custom_size, custom_hash)  // add more as you like 	*/
+	|| check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4")  // 5ec1cff/KernelSU
+	|| check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588")  // KOWX712/KernelSU
 	);
 }
