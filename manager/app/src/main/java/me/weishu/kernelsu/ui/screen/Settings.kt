df62f81b2c3f5dbe17bc84aef6b52055bf06a72b
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2024-12-30 16:38:43 +0800

    manager: remove shrink image

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Settings.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Settings.kt
@@ -62,7 +62,6 @@ import me.weishu.kernelsu.ui.component.UninstallDialog
 import me.weishu.kernelsu.ui.component.rememberConfirmDialog
 import me.weishu.kernelsu.ui.component.rememberLoadingDialog
 import me.weishu.kernelsu.ui.util.execKsud
-import me.weishu.kernelsu.ui.util.shrinkModules
 import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.Icon
 import top.yukonga.miuix.kmp.basic.IconButton
@@ -99,7 +98,6 @@ fun SettingScreen(navigator: DestinationsNavigator) {
         contentWindowInsets = WindowInsets.systemBars.add(WindowInsets.displayCutout).only(WindowInsetsSides.Horizontal)
     ) { innerPadding ->
         val loadingDialog = rememberLoadingDialog()
-        val shrinkDialog = rememberConfirmDialog()
 
         val showUninstallDialog = rememberSaveable { mutableStateOf(false) }
         val uninstallDialog = UninstallDialog(showUninstallDialog, navigator)
@@ -412,35 +410,12 @@ fun SettingScreen(navigator: DestinationsNavigator) {
                     }
                 }
 
-                val shrink = stringResource(id = R.string.shrink_sparse_image)
                 KsuIsValid {
                     Card(
                         modifier = Modifier
                             .padding(top = 12.dp)
                             .fillMaxWidth(),
                     ) {
-                        SuperArrow(
-                            title = shrink,
-                            leftAction = {
-                                Icon(
-                                    Icons.Rounded.Compress,
-                                    modifier = Modifier.padding(end = 16.dp),
-                                    contentDescription = shrink,
-                                    tint = colorScheme.onBackground
-                                )
-                            },
-                            onClick = {
-                                scope.launch {
-                                    val result = shrinkDialog.awaitConfirm(title = shrink)
-                                    if (result == ConfirmResult.Confirmed) {
-                                        loadingDialog.withLoading {
-                                            shrinkModules()
-                                        }
-                                    }
-                                }
-                            },
-                        )
-
                         val lkmMode = Natives.isLkmMode
                         if (lkmMode) {
                             val uninstall = stringResource(id = R.string.settings_uninstall)
