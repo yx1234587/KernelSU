376b9660a40ab34db99827a58340b546c7f06405
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-10 17:08:08 +0800

    kernel: ksu: printout quirks / backports / etc on init
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -2,6 +2,8 @@
 #include <linux/fs.h>
 #include <linux/kobject.h>
 #include <linux/module.h>
+#include <generated/utsrelease.h>
+#include <generated/compile.h>
 #include <linux/version.h> /* LINUX_VERSION_CODE, KERNEL_VERSION macros */
 #include <linux/workqueue.h>
 
@@ -28,8 +30,87 @@ bool ksu_queue_work(struct work_struct *work)
 
 extern void ksu_supercalls_init();
 
+// track backports and other quirks here
+// ref: kernel_compat.c, Makefile
+// yes looks nasty
+#if defined(CONFIG_KSU_KPROBES_KSUD)
+	#define FEAT_1 " +kprobes_ksud"
+#else
+	#define FEAT_1 ""
+#endif
+
+#if defined(CONFIG_KSU_KRETPROBES_SUCOMPAT)
+	#define FEAT_2 " +kretprobes_sucompat"
+#else
+	#define FEAT_2 ""
+#endif
+#if defined(CONFIG_KSU_THRONE_TRACKER_ALWAYS_THREADED)
+	#define FEAT_3 " +throne_always_threaded"
+#else
+	#define FEAT_3 ""
+#endif
+#if defined(CONFIG_KSU_EXTRAS)
+	#define FEAT_4 " +extras"
+#else
+	#define FEAT_4 ""
+#endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0) && !defined(CONFIG_KSU_LSM_SECURITY_HOOKS)
+	#define FEAT_5 " -lsm_hooks"
+#else
+	#define FEAT_5 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)) && defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
+	#define FEAT_6 " +allowlist_workaround"
+#else
+	#define FEAT_6 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0)) && defined(KSU_HAS_MODERN_EXT4)
+	#define FEAT_7 " +ext4_unregister_sysfs"
+#else
+	#define FEAT_7 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)) && defined(KSU_HAS_PATH_UMOUNT)
+	#define FEAT_8 " +path_umount"
+#else
+	#define FEAT_8 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 8, 0)) && defined(KSU_COPY_FROM_USER_NOFAULT)
+	#define FEAT_9 " +copy_from_user_nofault"
+#else
+	#define FEAT_9 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 3, 0)) && defined(KSU_PROBE_USER_READ)
+	#define FEAT_10 " +probe_user_read"
+#else
+	#define FEAT_10 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)) && defined(KSU_NEW_KERNEL_READ)
+	#define FEAT_11 " +new_kernel_read"
+#else
+	#define FEAT_11 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)) && defined(KSU_NEW_KERNEL_WRITE)
+	#define FEAT_12 " +new_kernel_write"
+#else
+	#define FEAT_12 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(3, 16, 0)) && defined(KSU_HAS_FOP_READ_ITER)
+	#define FEAT_13 " +read_iter"
+#else
+	#define FEAT_13 ""
+#endif
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 11, 0) && defined(KSU_HAS_ITERATE_DIR)
+	#define FEAT_14 " +iterate_dir"
+#else
+	#define FEAT_14 ""
+#endif
+
+#define EXTRA_FEATURES FEAT_1 FEAT_2 FEAT_3 FEAT_4 FEAT_5 FEAT_6 FEAT_7 FEAT_8 FEAT_9 FEAT_10 FEAT_11 FEAT_12 FEAT_13 FEAT_14
+
 int __init kernelsu_init(void)
 {
+	pr_info("Initialized on: %s (%s) with ksuver: %s%s\n", UTS_RELEASE, UTS_MACHINE, __stringify(KSU_VERSION), EXTRA_FEATURES);
+
 #ifdef CONFIG_KSU_DEBUG
 	pr_alert("*************************************************************");
 	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
