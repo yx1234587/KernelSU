42d226dc58ffc2dc0aa2e6ac3ad82ac919ab5248
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-13 18:59:08 +0800

    kernel: throne_tracker: resolve s_magic for < 3.9
    throne_tracker, cross-fs avoidance:
    f_inode is f_path.dentry->d_inode
    so file->f_inode->i_sb->s_magic is file->f_path.dentry->d_inode->i_sb->s_magic
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/throne_tracker.c
+++ kernel/throne_tracker.c
@@ -217,6 +217,13 @@ FILLDIR_RETURN_TYPE my_actor(struct dir_context *ctx, const char *name,
 	return FILLDIR_ACTOR_CONTINUE;
 }
 
+// compat: https://elixir.bootlin.com/linux/v3.9/source/include/linux/fs.h#L771
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,9,0)
+#define S_MAGIC_COMPAT(x) ((x)->f_inode->i_sb->s_magic)
+#else
+#define S_MAGIC_COMPAT(x) ((x)->f_path.dentry->d_inode->i_sb->s_magic)
+#endif
+
 void search_manager(const char *path, int depth, struct list_head *uid_data)
 {
 	int i, stop = 0;
@@ -257,8 +264,8 @@ void search_manager(const char *path, int depth, struct list_head *uid_data)
 				
 				// grab magic on first folder, which is /data/app
 				if (!data_app_magic) {
-					if (file->f_inode->i_sb->s_magic) {
-						data_app_magic = file->f_inode->i_sb->s_magic;
+					if (S_MAGIC_COMPAT(file)) {
+						data_app_magic = S_MAGIC_COMPAT(file);
 						pr_info("%s: dir: %s got magic! 0x%lx\n", __func__, pos->dirpath, data_app_magic);
 					} else {
 						filp_close(file, NULL);
@@ -266,9 +273,9 @@ void search_manager(const char *path, int depth, struct list_head *uid_data)
 					}
 				}
 				
-				if (file->f_inode->i_sb->s_magic != data_app_magic) {
+				if (S_MAGIC_COMPAT(file) != data_app_magic) {
 					pr_info("%s: skip: %s magic: 0x%lx expected: 0x%lx\n", __func__, pos->dirpath, 
-						file->f_inode->i_sb->s_magic, data_app_magic);
+						S_MAGIC_COMPAT(file), data_app_magic);
 					filp_close(file, NULL);
 					goto skip_iterate;
 				}
