aa6950477334580c5ae0f827b71053731e786d4d
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-25 23:42:15 +0800

    kernel: rp_sucompat: add kretprobes-hooked getname_flags for sucompat
    This introduces a kretprobe on getname_flags that improves the stealth
    and reliability of sucompat feature.
    
    Changes:
    - CONFIG_KSU_KRETPROBES_SUCOMPAT option to enable this hooking method
    - Hooks getname_flags() via kretprobe to intercept and modify filename->name
      on the return
    - prevent timing-based detections since it avoids individual syscall hijacking
      (newfstat vs newfstatat timing detections)
    - prevents doing usercopies, which in turn increases reliability on pagefaulty moments
    
    This allows sucompat to operate against anti-root detection techniques known as
    - Delayed syscall - KSU (ND)
    - sucompat SCA (Discolusre)
    - Abnormal Environment (NT)
    
    This is still very experimental, so default n, but yeah, it works.
    
    Related:
    - https://github.com/backslashxx/KernelSU/issues/5#issuecomment-2676829117
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/sucompat.c
+++ kernel/sucompat.c
@@ -254,14 +254,25 @@ int ksu_getname_flags_kernel(char **kname, int flags)
 	return ksu_sucompat_kernel_common((void *)*kname, "getname_flags", !!!flags);
 }
 
+#ifdef CONFIG_KSU_KRETPROBES_SUCOMPAT
+extern void rp_sucompat_exit();
+extern void rp_sucompat_init();
+#endif
+
 void ksu_sucompat_enable()
 {
+#ifdef CONFIG_KSU_KRETPROBES_SUCOMPAT
+	rp_sucompat_init();
+#endif
 	ksu_sucompat_enabled = true;
 	pr_info("%s: hooks enabled: exec, faccessat, stat\n", __func__);
 }
 
 void ksu_sucompat_disable()
 {
+#ifdef CONFIG_KSU_KRETPROBES_SUCOMPAT
+	rp_sucompat_exit();
+#endif
 	ksu_sucompat_enabled = false;
 	pr_info("%s: hooks disabled: exec, faccessat, stat\n", __func__);
 }
