360eecfac06259ca9a8ad4ec59ede739b4dc6b6f
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-08 19:35:38 +0800

    kernel: compat: provide weak anon_inode_getfd_secure for < 5.12
    int anon_inode_getfd(const char *name, const struct file_operations *fops,
    		     void *priv, int flags)
    
    int anon_inode_getfd_secure(const char *name, const struct file_operations *fops,
    			    void *priv, int flags,
    			    const struct inode *context_inode)
    
    this is on supercalls
    and the last argument is NULL anyway.
    will prolly work, no idea what the implications are.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -75,4 +75,13 @@ static inline struct inode *file_inode(struct file *f)
 }
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0)
+__weak int anon_inode_getfd_secure(const char *name, const struct file_operations *fops,
+			    void *priv, int flags,
+			    const struct inode *context_inode)
+{
+	return anon_inode_getfd(name, fops, priv, flags);
+}
+#endif
+
 #endif
